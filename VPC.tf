24c7cc1e3340318b45e7e27e7a74bad0169a9c02
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-10-07 11:37:20 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,44 +1,82 @@
-resource "aws_vpc" "vnet" {
+resource "aws_vpc" "network" {
     cidr_block = "192.168.0.0/16"
-
     tags = {
-        Name = "VPC-Demo"
+        Name = "VPC-40"
     }
 }
 
-resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.vnet.id
-    cidr_block = "192.168.1.0/24"
+resource "aws_subnet" "sub1" {
+    vpc_id  = aws_vpc.network.id
+    cidr_block = "192.168.0.0/22"
     availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = "true"
-
-    tags ={
-        Name = "Public-Subnet"
-    }
+    map_public_ip_on_launch = true
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vnet.id
-
+resource "aws_internet_gateway" "int" {
+    vpc_id = aws_vpc.network.id 
     tags = {
-      Name = "Internet-gw"
+        Name = "igw-vpc-40"
     }
+  
 }
 
-resource "aws_route_table" "rt" {
-    vpc_id = aws_vpc.vnet.id
+resource "aws_route_table" "Public" {
+    vpc_id = aws_vpc.network.id
     tags = {
-        Name = "RT-Public"
+        Name = "Public-RT-40"
     }
 
-    route {
-       gateway_id = aws_internet_gateway.igw.id
-       cidr_block = "0.0.0.0/0"
+        route {
+            gateway_id = aws_internet_gateway.int.id
+            cidr_block = "0.0.0.0/0"
     }
+  
 }
 
-resource "aws_route_table_association" "attach" {
-    route_table_id = aws_route_table.rt.id
-    subnet_id = aws_subnet.public.id
+resource "aws_route_table_association" "demo" {
+    subnet_id = aws_subnet.sub1.id
+    route_table_id = aws_route_table.Public.id
 }
 
+resource "aws_security_group" "sg" {
+
+    vpc_id = aws_vpc.network.id
+    tags = {
+
+        Name = "sg-40"
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+    }
+  
+   egress {
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "-1"
+   }
+}
+
+
+resource "aws_instance" "vm" {
+    ami = "ami-078264b8ba71bc45e"
+    instance_type = "t2.micro"
+    key_name = "netflixe"
+    subnet_id = aws_subnet.sub1.id
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    tags = {
+        Name = "Webserver"
+    }
+  
+}
