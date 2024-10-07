979a8b1a342399007f48763306c08e6c4be06caa
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-10-07 12:00:15 +0530

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -10,7 +10,24 @@ resource "aws_subnet" "sub1" {
     cidr_block = "192.168.0.0/22"
     availability_zone = "ap-south-1a"
     map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Public-Subnet"
+    }
 }
+
+resource "aws_subnet" "sub2" {
+    vpc_id = aws_vpc.network.id
+    cidr_block = "192.168.4.0/22"
+    availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = false
+
+      tags = {
+        Name = "Private-Subnet"
+    }
+  
+}
+
 
 resource "aws_internet_gateway" "int" {
     vpc_id = aws_vpc.network.id 
@@ -19,6 +36,35 @@ resource "aws_internet_gateway" "int" {
     }
   
 }
+
+
+resource "aws_eip" "static" {
+    domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+    subnet_id = aws_subnet.sub1.id
+    allocation_id = aws_eip.static.id
+  
+}
+
+resource "aws_route_table" "Private" {
+    vpc_id = aws_vpc.network.id
+    tags = {
+        Name = "Private-RT-40"
+    }
+        route {
+          cidr_block = "0.0.0.0/0"
+          nat_gateway_id = aws_nat_gateway.nat.id
+        }
+    }
+  
+  resource "aws_route_table_association" "demo1" {
+    subnet_id = aws_subnet.sub2.id
+    route_table_id = aws_route_table.Private.id
+    
+  }
+
 
 resource "aws_route_table" "Public" {
     vpc_id = aws_vpc.network.id
@@ -77,6 +123,18 @@ resource "aws_instance" "vm" {
     vpc_security_group_ids = [aws_security_group.sg.id]
     tags = {
         Name = "Webserver"
+    }
+  
+}
+
+resource "aws_instance" "vm-1" {
+    ami = "ami-078264b8ba71bc45e"
+     instance_type = "t2.micro"
+    key_name = "netflixe"
+    subnet_id = aws_subnet.sub2.id
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    tags = {
+        Name = "Private-Instance"
     }
   
 }
