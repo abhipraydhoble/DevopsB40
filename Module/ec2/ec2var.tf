462f9068393d33e71264f3b17d89c40a2226d4fd
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-10-11 11:52:32 +0530

    Create ec2var.tf

---
+++ Module/ec2/ec2var.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
