bc42983b1d324fedba0adccf5da7210bb0325458
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-01-21 12:01:40 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -3,3 +3,17 @@ provider "aws" {
     access_key = "AKIA23WHUEH4HOS5Gsdfghj"
     secret_key = "STShGfXudQ06GyFmeBd1h3biTXimpUIU"
 }
+
+
+# provider block with cli profile
+# install aws cli
+# aws configure  --profile tf-user
+# enter access_key: 
+# enter secret_key:
+# enter region:
+
+
+provider "aws"  {
+   region = "ap-southeast-1"
+   profile = "tf-user"
+}
