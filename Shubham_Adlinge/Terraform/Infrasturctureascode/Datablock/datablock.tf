735957ea4e6abd9e3d0ec6fcc436104c646f2f13
SHUBHAM ADLINGE <124245410+shubhamadlinge@users.noreply.github.com>
2025-01-26 12:23:33 +0530

    Update datablock.tf

--- Shubham_Adlinge/Terraform/Infrasturctureascode/Datablock/datablock.tf
+++ Shubham_Adlinge/Terraform/Infrasturctureascode/Datablock/datablock.tf
@@ -1,1 +1,20 @@
+data "aws_ami" "latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.latest.id
+  instance_type = "t2.micro"
+}
+
+### calling the data block in instance
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.latest.id
+  instance_type = "t2.micro"
+}
 
