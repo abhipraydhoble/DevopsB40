959a63c1f5adc0b858d26ff4f79ec6536c4b1ee4
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-01-27 11:43:09 +0530

    Create data.tf

---
+++ Terraform/data.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "example" {
+  most_recent      = true 
+  owners           = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.6.20250123.4-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "vm" {
+    ami = data.aws_ami.example.id
+    instance_type = "t3.micro"
+    count = 3                               # create no of instances
+    key_name = "3tier"
+    tags = {
+       Name = "Server- ${count.index}"      # create 3 instancces with name server-0 server-1 server-2
+    }
+     user_data = <<-EOF
+    #!/bin/bash
+    sudo -i
+    yum update -y
+    yum install nginx -y
+    systemctl start nginx 
+    systemctl enable nginx 
+    EOF
+
+}
+
